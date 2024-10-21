68f0e27f555f4f1316676db6275fed4940163407
quocnhut1010 <128989079+quocnhut1010@users.noreply.github.com>
2024-10-22 00:01:04 +0700

    update dang ky vs chuyen canh khi bam logout

--- app/src/main/java/com/example/ckqlct/Bottom_fragment/StaticticalFragment.java
+++ app/src/main/java/com/example/ckqlct/Bottom_fragment/StaticticalFragment.java
@@ -54,6 +54,7 @@ public class StaticticalFragment extends Fragment {
         if (getArguments () != null) {
             mParam1 = getArguments ().getString (ARG_PARAM1);
             mParam2 = getArguments ().getString (ARG_PARAM2);
+
         }
     }
 

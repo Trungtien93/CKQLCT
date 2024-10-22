d8af24c72cd1d07000a7363cf02fcc1546c529f2
namngxthanh <namngx163@gmail.com>
2024-10-22 09:37:37 +0700

    change Greeting text

--- app/src/main/java/com/example/ckqlct/Bottom_fragment/HomeFragment.java
+++ app/src/main/java/com/example/ckqlct/Bottom_fragment/HomeFragment.java
@@ -1,59 +1,36 @@
 package com.example.ckqlct.Bottom_fragment;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import com.example.ckqlct.R;
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView greetingText;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment ();
-        Bundle args = new Bundle ();
-        args.putString (ARG_PARAM1, param1);
-        args.putString (ARG_PARAM2, param2);
-        fragment.setArguments (args);
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString("param1", param1);
+        args.putString("param2", param2);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        if (getArguments () != null) {
-            mParam1 = getArguments ().getString (ARG_PARAM1);
-            mParam2 = getArguments ().getString (ARG_PARAM2);
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            String mParam1 = getArguments().getString("param1");
+            String mParam2 = getArguments().getString("param2");
         }
     }
 
@@ -61,6 +38,26 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate (R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Find the TextView after the layout is inflated
+        greetingText = view.findViewById(R.id.greetingText);
+
+        // Set the greeting message based on the time of day
+        Calendar calendar = Calendar.getInstance();
+        int time = calendar.get(Calendar.HOUR_OF_DAY);
+        if (time >= 0 && time < 12) {
+            greetingText.setText("Buổi sáng tốt lành nhé!");
+        } else if (time >= 12 && time < 16) {
+            greetingText.setText("Buổi trưa mát mẻ nha");
+        } else if (time >= 16 && time < 21) {
+            greetingText.setText("Buổi chiều vui vẻ nhe");
+        } else if (time >= 21 && time < 24) {
+            greetingText.setText("Chúc bạn ngủ ngon!");
+        } else {
+            greetingText.setText("Xin chào người dùng");
+        }
+
+        return view;
     }
 }
